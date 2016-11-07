cefa01e0914ae4f51d51a08e979701bbf6ad90d3
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-11-07 09:17:37 -0800

    window added

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,70 +1,72 @@
-
+(function(module) {
 //creats a project odject made from the content in projects_list.js
-function Projects(obj) {
-  for (var keys in obj) {
-    this[keys] = obj[keys];
+  function Projects(obj) {
+    for (var keys in obj) {
+      this[keys] = obj[keys];
+    }
   }
-}
 
-Projects.theProjects = [];
+  Projects.theProjects = [];
 
-//adds projects content to the handlebars in the html
-Projects.prototype.toHtml = function() {
-  var $newProject = $('#local-data-template').html();
-  var projectRender = Handlebars.compile($newProject);
-  return projectRender(this);
-};
+  //adds projects content to the handlebars in the html
+  Projects.prototype.toHtml = function() {
+    var $newProject = $('#local-data-template').html();
+    var projectRender = Handlebars.compile($newProject);
+    return projectRender(this);
+  };
 
-//creates a new project object
-Projects.loadAll = function(inputData) {
-  inputData.forEach(function(ele) {
-    Projects.theProjects.push(new Projects(ele));
-  });
-};
+  //creates a new project object
+  Projects.loadAll = function(inputData) {
+    inputData.forEach(function(ele) {
+      Projects.theProjects.push(new Projects(ele));
+    });
+  };
 
-Projects.fetchAll = function() {
-  if (localStorage.projects_list) {
-    $.ajax({
-      url: 'data/projects_list.json',
-      type: 'HEAD',
-      success: function(data, msg, xhr) {
-        var parseEtag = jQuery.parseJSON(localStorage.etag);
-        if(parseEtag !== xhr.getResponseHeader('ETag')) {
-          $.getJSON('data/projects_list.json').done(function(data, msg, xhr) {
-            localStorage.projects_list = JSON.stringify(data);
-            var etag = xhr.getResponseHeader('ETag');
-            localStorage.etag = JSON.stringify(etag);
-            Projects.loadAll(data);
-          });
-        } else {
-          Projects.loadAll($.parseJSON(localStorage.projects_list));
+  Projects.fetchAll = function() {
+    if (localStorage.projects_list) {
+      $.ajax({
+        url: 'data/projects_list.json',
+        type: 'HEAD',
+        success: function(data, msg, xhr) {
+          var parseEtag = jQuery.parseJSON(localStorage.etag);
+          if(parseEtag !== xhr.getResponseHeader('ETag')) {
+            $.getJSON('data/projects_list.json').done(function(data, msg, xhr) {
+              localStorage.projects_list = JSON.stringify(data);
+              var etag = xhr.getResponseHeader('ETag');
+              localStorage.etag = JSON.stringify(etag);
+              Projects.loadAll(data);
+            });
+          } else {
+            Projects.loadAll($.parseJSON(localStorage.projects_list));
+          }
         }
-      }
-    });
-  } else {
-    $.getJSON('data/projects_list.json').done(function(data, msg, xhr) {
-      localStorage.projects_list = JSON.stringify(data);
-      var etag = xhr.getResponseHeader('ETag');
-      localStorage.etag = JSON.stringify(etag);
-      Projects.loadAll(data);
-    });
-  }
-};
-Projects.fetchAll();
+      });
+    } else {
+      $.getJSON('data/projects_list.json').done(function(data, msg, xhr) {
+        localStorage.projects_list = JSON.stringify(data);
+        var etag = xhr.getResponseHeader('ETag');
+        localStorage.etag = JSON.stringify(etag);
+        Projects.loadAll(data);
+      });
+    }
+  };
+  Projects.fetchAll();
 
-Projects.numWordsAll = function() {
-  return Projects.theProjects.map(function(project) {
-    return project.description.split(' ').length;
-  })
-    .reduce(function(curr, next, index, array) {
-      return curr + next;
-    }, 0);
-};
+  Projects.numWordsAll = function() {
+    return Projects.theProjects.map(function(project) {
+      return project.description.split(' ').length;
+    })
+      .reduce(function(curr, next, index, array) {
+        return curr + next;
+      }, 0);
+  };
 
-//adds the projects content to the display field
-$('section.projects').mouseenter(function() {
-  $('.projDisplay').append('h4').text('Number of words ' + Projects.numWordsAll());
-  Projects.theProjects.forEach(function(project) {
-    $('.projDisplay').append(project.toHtml());
+  //adds the projects content to the display field
+  $('section.projects').mouseenter(function() {
+    $('.projDisplay').append('h4').text('Number of words ' + Projects.numWordsAll());
+    Projects.theProjects.forEach(function(project) {
+      $('.projDisplay').append(project.toHtml());
+    });
   });
-});
+  module.Projects = Projects;
+})(window);
